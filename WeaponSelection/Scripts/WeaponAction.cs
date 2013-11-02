6e598838a59dce6e72893905e6a7ba78b3625a58
Andrew Fray <andrew@spryfox.com>
2013-11-02 23:09:56 +0000

    whoops badly-named weapon action

--- WeaponSelection/Scripts/WeaponAction.cs
+++ WeaponSelection/Scripts/WeaponAction.cs
@@ -9,8 +9,7 @@ namespace TenPN.DecisionFlex.Demos
         public override void Perform(IConsiderationContext context)
         {
             var target = context.GetContext<GameObject>("Enemy");
-            string label = m_weaponVerb + " ";
-            LabelLerper.LerpLabelFromTo(label, transform.position, target.transform);
+            LabelLerper.LerpLabelFromTo(m_weaponVerb, transform.position, target.transform);
         }
 
         //////////////////////////////////////////////////
