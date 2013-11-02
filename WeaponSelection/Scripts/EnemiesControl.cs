77f90824622723fcc127da706c078ecb972b42e9
Andrew Fray <andrew@spryfox.com>
2013-11-02 23:49:33 +0000

    script to control enemies through on-screen GUI

---
+++ WeaponSelection/Scripts/EnemiesControl.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace TenPN.DecisionFlex.Demos
+{
+    public class EnemiesControl : MonoBehaviour
+    {
+        //////////////////////////////////////////////////
+        
+        bool m_isPaused;
+        IEnumerable<FollowWaypoints> m_enemies;
+
+        //////////////////////////////////////////////////
+
+        void Awake()
+        {
+            m_enemies = GetComponentsInChildren<FollowWaypoints>();
+        }
+        
+        void OnGUI()
+        {
+            var togglePauseRect = new Rect(20f, 20f,
+                                             400f, 40f);
+            var newIsPaused = GUI.Toggle(togglePauseRect, m_isPaused, "Pause enemies");
+            if (newIsPaused != m_isPaused)
+            {
+                foreach(var enemy in m_enemies)
+                {
+                    enemy.IsPaused = newIsPaused;
+                }
+                m_isPaused = newIsPaused;
+            }
+        }
+    }
+}
