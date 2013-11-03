6f613b02feb74d8d1c210410284f0c966b1c4ea2
Andrew Fray <andrew@spryfox.com>
2013-11-03 00:24:37 +0000

    inventory UI to tweak ammo counts at runtime

--- WeaponSelection/Scripts/Inventory.cs
+++ WeaponSelection/Scripts/Inventory.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System;
 using System.Reflection;
+using System.Collections.Generic;
 
 namespace TenPN.DecisionFlex.Demos
 {
@@ -14,24 +15,95 @@ namespace TenPN.DecisionFlex.Demos
 
         //////////////////////////////////////////////////
 
-        // push our relevent into into master context
-        private void PopulateMasterConsiderationContext(IConsiderationContext masterContext)
+        private struct Ammo
         {
+            public string Name;
+
+            public int GetCount()
+            {
+                return (int) Field.GetValue(Inventory);
+            }
+
+            public void SetCount(int newCount)
+            {
+                Field.SetValue(Inventory, newCount);
+            }
+
+            public FieldInfo Field;
+            public Inventory Inventory;
+        }
+
+        private IEnumerable<Ammo> CalculateInventoryContents()
+        {
+            var contents = new List<Ammo>();
+
             var allInventoryFields = GetType().GetFields(
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             foreach(var inventoryField in allInventoryFields)
             {
-                int inventoryValue = (int)inventoryField.GetValue(this);
-
                 string contextName = inventoryField.Name.StartsWith("m_")
                     ? inventoryField.Name.Substring(2)
                     : inventoryField.Name;
 
-                masterContext.SetContext(contextName,
-                                         inventoryValue);
+                contents.Add(new Ammo {
+                        Name = contextName,
+                        Field = inventoryField,
+                        Inventory = this,
+                    });
             }
 
+            return contents;
+        }
+
+        // push our relevent into into master context
+        private void PopulateMasterConsiderationContext(IConsiderationContext masterContext)
+        {
+            var contents = CalculateInventoryContents();
+
+            foreach(var ammo in contents)
+            {
+                masterContext.SetContext(ammo.Name, ammo.GetCount());
+            }
+        }
+
+        private void OnGUI()
+        {
+            var soldierScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+            soldierScreenPos.y = Screen.height - soldierScreenPos.y;
+
+            float inventoryUIHeight = 80f;
+            float inventoryUIWidth = 200f;
+            float inventoryUIRightOffset = 30f;
+
+            var inventoryUIRect = new Rect(soldierScreenPos.x + inventoryUIRightOffset, 
+                                           soldierScreenPos.y - inventoryUIHeight,
+                                           inventoryUIWidth, inventoryUIHeight);
+
+            GUILayout.BeginArea(inventoryUIRect, GUI.skin.box);
+
+            GUILayout.Label("Ammo");
+            GUILayout.FlexibleSpace();
+
+            var contents = CalculateInventoryContents();
+            foreach(var ammo in contents)
+            {
+                GUILayout.BeginHorizontal();
+
+                int currentCount = ammo.GetCount();
+                GUILayout.Label(ammo.Name + ": " + currentCount);
+
+                int minAmmo = 0;
+                int maxAmmo = 20;
+                int newCount = (int)GUILayout.HorizontalSlider(currentCount, 
+                                                               minAmmo, maxAmmo);
+                ammo.SetCount(newCount);
+                                
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.EndArea();
+            
         }
     }
 }
