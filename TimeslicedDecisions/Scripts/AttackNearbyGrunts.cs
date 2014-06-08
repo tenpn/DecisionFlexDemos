d359fc1acdd41db8d893b18b08e284c72d669ebb
Andrew Fray <andrew@spryfox.com>
2014-06-08 16:09:51 +0100

    AttackNearbyGrunts enemy script

---
+++ TimeslicedDecisions/Scripts/AttackNearbyGrunts.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+namespace TenPN.DecisionFlex.Demos
+{
+    [AddComponentMenu("TenPN/DecisionFlex/Demos/Timesliced/AttackNearbyGrunts")]
+    public class AttackNearbyGrunts : MonoBehaviour
+    {
+        //////////////////////////////////////////////////
+
+        private static readonly int s_maxGrunts = 99;
+        private Collider2D[] m_gruntCache = new Collider2D[s_maxGrunts];
+        private CircleCollider2D m_attackZone;
+        private float m_timeToNextAttack = 0f;
+
+        [SerializeField]
+        private float m_attackStrength = 5f;
+
+        [SerializeField]
+        private float m_attackInterval = 1f;
+
+        //////////////////////////////////////////////////
+
+        void Start()
+        {
+            m_attackZone = GetComponent<CircleCollider2D>();
+        }
+
+        void Update()
+        {
+            m_timeToNextAttack -= Time.deltaTime;
+            while (m_timeToNextAttack < 0f) 
+            {
+                var target = FindGruntToAttack();
+                if (target != null)
+                {
+                    AttackGrunt(target);
+                }
+                m_timeToNextAttack += m_attackInterval;
+            }
+        }
+        
+        // returns null if no target
+        Transform FindGruntToAttack()
+        {
+            int gruntMask = 1 << LayerMask.NameToLayer("Grunt");
+            float attackRadius = m_attackZone.radius;
+            int nearbyGruntCount = 
+                Physics2D.OverlapCircleNonAlloc(transform.position,
+                                                attackRadius,
+                                                m_gruntCache,
+                                                gruntMask);
+
+            if (nearbyGruntCount == 0)
+            {
+                return null;
+            }
+            else
+            {
+                int victimIndex = Random.Range(0, nearbyGruntCount);
+                return m_gruntCache[victimIndex].transform;
+            }
+        }
+
+        void AttackGrunt(Transform victimTransform)
+        {
+            var victim = victimTransform.GetComponent<Grunt>();
+            victim.HP -= m_attackStrength;
+            Debug.DrawLine(transform.position, victimTransform.position, Color.red);
+        }
+    }
+}
