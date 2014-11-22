3386fd952239640363b0a1d457abf5b6861bb1b5
Andrew Fray <andrew.fray@gmail.com>
2014-11-22 13:56:12 +0000

    iPerson first in menu, because that's a gentler introduction

--- MainMenu.cs
+++ MainMenu.cs
@@ -62,8 +62,8 @@ namespace TenPN.DecisionFlex.Demos
 
             var loadLevelLayout = GUILayout.MinWidth(menuWidth * 0.9f);
             var levelNames = new string[] { 
-                "DecisionFlex_WeaponSelection", 
                 "DecisionFlex_iPerson", 
+                "DecisionFlex_WeaponSelection", 
                 "DecisionFlex_UFOs" 
             };
             foreach(var levelName in levelNames)
